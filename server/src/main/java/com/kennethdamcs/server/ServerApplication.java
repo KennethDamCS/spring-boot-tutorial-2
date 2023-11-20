6bed918b3f2f3e81cde38e7240b03429246a1f2d
Kenneth Dam <mrdam.kiet@gmail.com>
2023-11-19 20:30:29 -0800

    Added controller class and command to populate database on start up

--- server/src/main/java/com/kennethdamcs/server/ServerApplication.java
+++ server/src/main/java/com/kennethdamcs/server/ServerApplication.java
@@ -1,5 +1,9 @@
 package com.kennethdamcs.server;
 
+import com.kennethdamcs.server.enumeration.Status;
+import com.kennethdamcs.server.model.Server;
+import com.kennethdamcs.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +14,16 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    CommandLineRunner run(ServerRepo serverRepo){
+        return args -> {
+            serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC",
+                            "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "32GB", "Dell Tower",
+                    "http://localhost:8080/server/image/server1.png", Status.SERVER_DOWN));
+            serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "64GB", "Web Server",
+                    "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "128GB", "Mail Server",
+                    "http://localhost:8080/server/image/server1.png", Status.SERVER_DOWN));
+        };
+    }
 }
--- server/src/main/java/com/kennethdamcs/server/resource/ServerResource.java
+++ server/src/main/java/com/kennethdamcs/server/resource/ServerResource.java
@@ -4,18 +4,20 @@ import com.kennethdamcs.server.enumeration.Status;
 import com.kennethdamcs.server.model.Response;
 import com.kennethdamcs.server.model.Server;
 import com.kennethdamcs.server.service.implementation.ServerServiceImplementation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/server")
@@ -48,5 +50,49 @@ public class ServerResource {
                         .statusCode(OK.value())
                         .build()
         );
+    }
+
+    @PostMapping ("/save")
+    public ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", serverService.create(server)))
+                        .message("Server created")
+                        .status(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", serverService.get(id)))
+                        .message("Server retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> deleteServer(@PathVariable("id") Long id){
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .message("Server deleted")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping (path = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
+    public byte[] deleteServer(@PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "repos/spring-boot-tutorial-2-images/" +fileName));
     }
 }
