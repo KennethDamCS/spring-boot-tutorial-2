b0c603e2e5f04e1038145ee57bbef9614e0c40b2
Kenneth Dam <mrdam.kiet@gmail.com>
2023-11-19 19:59:54 -0800

    RESTful api creation

---
+++ server/src/main/java/com/kennethdamcs/server/resource/ServerResource.java
@@ -0,0 +1,52 @@
+package com.kennethdamcs.server.resource;
+
+import com.kennethdamcs.server.enumeration.Status;
+import com.kennethdamcs.server.model.Response;
+import com.kennethdamcs.server.model.Server;
+import com.kennethdamcs.server.service.implementation.ServerServiceImplementation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/server")
+@RequiredArgsConstructor
+public class ServerResource {
+    private final ServerServiceImplementation serverService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Response> getServers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("servers", server))
+                        .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
--- server/src/main/java/com/kennethdamcs/server/service/implementation/ServerServiceImplementation.java
+++ server/src/main/java/com/kennethdamcs/server/service/implementation/ServerServiceImplementation.java
@@ -6,9 +6,20 @@ import com.kennethdamcs.server.service.ServerService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
+import java.util.Random;
+
+import static com.kennethdamcs.server.enumeration.Status.SERVER_DOWN;
+import static com.kennethdamcs.server.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
+import static org.springframework.data.domain.PageRequest.of;
 
 @RequiredArgsConstructor
 @Service
@@ -18,31 +29,49 @@ public class ServerServiceImplementation implements ServerService {
     private final ServerRepo serverRepo;
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
+
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by ID: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by ID: {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
+    }
+
+    private String setServerImageUrl() {
+        String[] imageNames = {"server1.jpg", "server2.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
