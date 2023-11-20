e1d6fbf2f6ba1ebe4d4334fe2d81135128cf8c26
Kenneth Dam <mrdam.kiet@gmail.com>
2023-11-19 21:04:49 -0800

    Config file and bug fixes

--- server/src/main/java/com/kennethdamcs/server/ServerApplication.java
+++ server/src/main/java/com/kennethdamcs/server/ServerApplication.java
@@ -6,6 +6,10 @@ import com.kennethdamcs.server.repo.ServerRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.kennethdamcs.server.enumeration.Status.SERVER_DOWN;
+import static com.kennethdamcs.server.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -14,16 +18,17 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @Bean
     CommandLineRunner run(ServerRepo serverRepo){
         return args -> {
             serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC",
-                            "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+                            "http://localhost:8080/server/image/server1.png", SERVER_DOWN));
             serverRepo.save(new Server(null, "192.168.1.58", "Fedora Linux", "32GB", "Dell Tower",
-                    "http://localhost:8080/server/image/server1.png", Status.SERVER_DOWN));
+                    "http://localhost:8080/server/image/server2.png", SERVER_DOWN));
             serverRepo.save(new Server(null, "192.168.1.21", "MS 2008", "64GB", "Web Server",
-                    "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+                    "http://localhost:8080/server/image/server3.png", SERVER_DOWN));
             serverRepo.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "128GB", "Mail Server",
-                    "http://localhost:8080/server/image/server1.png", Status.SERVER_DOWN));
+                    "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
         };
     }
 }
