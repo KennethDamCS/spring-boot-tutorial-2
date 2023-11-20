b2f4b1eedbe720e9846b0dcc040d80562a9bdec4
Kenneth Dam <mrdam.kiet@gmail.com>
2023-11-20 14:47:44 -0800

    More service methods

--- serverapp/src/app/service/server.service.ts
+++ serverapp/src/app/service/server.service.ts
@@ -1,51 +1,82 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpErrorResponse,
+} from '@angular/common/http';
 import { Observable, catchError, tap, throwError } from 'rxjs';
 import { CustomeResponse } from '../interface/custom-response';
 import { Server } from '../interface/server';
+import { Status } from '../enum/status.enum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServerService {
   private apiUrl = 'any';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // getServers(): Observable<CustomeResponse>{
   //   return this.http.get<CustomeResponse>(`http://localhost:8080/server/list`);
   // }
 
-  servers$ = <Observable<CustomeResponse>>
-  this.http.get<CustomeResponse>(`${this.apiUrl}/server/list`)
-  .pipe(
-    tap(console.log),
-    catchError(this.handleError)
+  servers$ = <Observable<CustomeResponse>>(
+    this.http
+      .get<CustomeResponse>(`${this.apiUrl}/server/list`)
+      .pipe(tap(console.log), catchError(this.handleError))
   );
 
-  save$ = (server: Server) => <Observable<CustomeResponse>>
-  this.http.post<CustomeResponse>(`${this.apiUrl}/server/save`, server)
-  .pipe(
-    tap(console.log),
-    catchError(this.handleError)
-  );
+  save$ = (server: Server) =>
+    <Observable<CustomeResponse>>(
+      this.http
+        .post<CustomeResponse>(`${this.apiUrl}/server/save`, server)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
 
-  ping$ = (ipAddress: string) => <Observable<CustomeResponse>>
-  this.http.get<CustomeResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
-  .pipe(
-    tap(console.log),
-    catchError(this.handleError)
-  );
+  ping$ = (ipAddress: string) =>
+    <Observable<CustomeResponse>>(
+      this.http
+        .get<CustomeResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
 
-  delete$ = (serverId: number) => <Observable<CustomeResponse>>
-  this.http.delete<CustomeResponse>(`${this.apiUrl}/server/delete/${serverId}`)
-  .pipe(
-    tap(console.log),
-    catchError(this.handleError)
-  );
+  filter$ = (status: Status, response: CustomeResponse) =>
+    <Observable<CustomeResponse>>new Observable<CustomeResponse>(
+      (subscriber) => {
+        console.log(response);
+        subscriber.next(
+          status === Status.ALL
+            ? { ...response, message: `Servers filtered by ${status} status` }
+            : {
+                ...response,
+                message:
+                  response.data.servers.filter(
+                    (server) => server.status === status
+                  ).length > 0
+                    ? `Servers filtered by 
+          ${status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'} status`
+                    : `No servers of ${status} found`,
+                data: {
+                  servers: response.data.servers.filter(
+                    (server) => server.status === status
+                  ),
+                },
+              }
+        );
+        subscriber.complete();
+      }
+    ).pipe(tap(console.log), catchError(this.handleError));
+
+  delete$ = (serverId: number) =>
+    <Observable<CustomeResponse>>(
+      this.http
+        .delete<CustomeResponse>(`${this.apiUrl}/server/delete/${serverId}`)
+        .pipe(tap(console.log), catchError(this.handleError))
+    );
 
   private handleError(error: HttpErrorResponse): Observable<never> {
     console.log(error);
-    return throwError(() => 'Method not implemented.');
+    return throwError(() => `An error occured - Error code: ${error.status}`);
   }
 }
